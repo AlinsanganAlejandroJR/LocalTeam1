067af4bc38bfb51bc0577cf6167f16fc974030f4
Alejandro <alejandro.softvision@.com>
2022-11-21 16:29:32 +0800

    main

---
+++ src/main/java/com/academy/LocalTeam1/LocalTeam1Application.java
@@ -0,0 +1,13 @@
+package com.academy.LocalTeam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalTeam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalTeam1Application.class, args);
+	}
+
+}
