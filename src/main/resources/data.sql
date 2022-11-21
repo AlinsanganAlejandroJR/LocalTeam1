966d248ca551c6f6d25822abaa2ec129e22baf0c
Alejandro <alejandro.softvision@.com>
2022-11-21 18:01:59 +0800

    Data Sql Value Modified

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,2 @@
 INSERT INTO community_manager (first_name,middle_name,last_name,is_active) VALUES ('ComFirstName1','ComMiddleName1', 'ComLastName1', 'TRUE');
 INSERT INTO community_manager (first_name,last_name,is_active) VALUES ('ComFirstName2', 'ComLastName1', 'FALSE');
-INSERT INTO community_manager (first_name,middle_name,last_name,is_active) VALUES ('ComFirstName3','ComMiddleName3', 'ComLastName3', 'FALSE');
